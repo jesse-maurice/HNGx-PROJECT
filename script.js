3c0f8fde1560ba2688daeec844d569ce14baa6e0
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-09-08 16:21:40 +0100

    final

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ const today = new Date();
 const currentDayOfTheWeek = daysOfWeek[today.getDay()];
 
 // Get the current UTC time
-const utcHours = today.getUTCHours().toString().padStart(2, "0");
-const utcMinutes = today.getUTCMinutes().toString().padStart(2, "0");
-const utcSeconds = today.getUTCSeconds().toString().padStart(2, "0");
+const utcTimeMilliseconds = today.getTime();
 
 // Display the day of the week and UTC time in the HTML
 document.getElementById("currentDayOfTheWeek").textContent =
   currentDayOfTheWeek;
 document.getElementById(
   "currrentUTCTime"
-).textContent = `${utcHours}:${utcMinutes}:${utcSeconds}`;
+).textContent = utcTimeMilliseconds;
